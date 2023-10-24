1ea2f21bb5bb615fa7dae4a68779beda6e3b2283
William <128476002+williamwili@users.noreply.github.com>
2023-10-24 15:34:55 +0700

    Add files via upload

---
+++ Karyawan/Require/Header.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Font Family -->
+    
+    
+    <!-- Material -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <title>Healthy Food!</title>
+  </head>
+  <body>
+      
+            <header class="header">
+                <div class="header-left">
+                    <div class="search-bar">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <span class="material-symbols-outlined"><a href="index.php">
+search
+</a></span>
+                    </div>
+                </div>
+
+                <div class="header-right">
+                <span class="material-symbols-outlined">
+                notifications
+                </span>
+                </div>
+        </header>
+
+  </body>
+</html>
---
+++ Karyawan/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+     <!-- Font Family -->
+     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+     
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <title>Healthy Food!</title>
+  </head>
+  <body>
+    <h1>Test</h1>
+  </body>
+</html>
